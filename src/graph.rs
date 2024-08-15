6d3e61626e53214392ce1dd0382a04c0b963260e
uri singer <urisig2007@gmail.com>
2024-08-15 22:08:18 +0300

    added lots of things, might switch to pest parser soon

--- src/graph.rs
+++ src/graph.rs
@@ -1,40 +1,99 @@
+use std::{collections::HashMap, fmt::Debug};
+
 use iced::{
     event::Status,
     mouse::{self, Cursor},
-    widget::canvas::{event, Event, Frame, Geometry, Path, Program, Stroke},
+    widget::canvas::{event, Cache, Event, Frame, Geometry, Path, Program, Stroke},
     Color, Point, Theme, Vector,
 };
 
-use crate::Message;
+use crate::{
+    latex::SubscriptSymbol,
+    parser::{tokenizer::tokenize, EvalError, Node},
+    Message,
+};
 
-pub struct Graph<F>
-where
-    F: Fn(f32) -> f32,
-{
-    pub f: F,
+pub struct Graph {
+    pub eq: Node,
+    graph_cache: Cache,
+}
 
+pub struct GraphRenderer {
     pub scale: f32,
     pub mid: Vector,
     pub resolution: u32,
+
+    pub graphs: Vec<Option<Graph>>,
+
+    symbols: HashMap<SubscriptSymbol, f32>,
 }
 
-impl<F> Graph<F>
-where
-    F: Fn(f32) -> f32,
-{
-    pub fn get_points(&self, range: f32) -> Vec<Vector> {
+impl GraphRenderer {
+    pub fn new(scale: f32, mid: Vector, resolution: u32) -> Self {
+        Self {
+            scale,
+            mid,
+            resolution,
+            graphs: Vec::new(),
+            symbols: HashMap::new(),
+        }
+    }
+
+    pub fn add_equation(&mut self, eq: &str) -> Result<(), String> {
+        self.graphs.push(None);
+        self.set_equation(self.graphs.len() - 1, eq)
+    }
+
+    pub fn set_equation(&mut self, i: usize, eq: &str) -> Result<(), String> {
+        let tokens = tokenize(eq.chars()).map_err(|e| e.to_string())?;
+        let parsed_eq = Node::parse(tokens).map_err(|e| e.to_string())?;
+        self.graphs[i] = Some(Graph::new(parsed_eq));
+        Ok(())
+    }
+
+    pub fn clear_caches(&self) {
+        for graph in &self.graphs {
+            graph.as_ref().map(|g| g.clear_cache());
+        }
+    }
+}
+
+impl Graph {
+    pub fn new(eq: Node) -> Self {
+        Self {
+            eq,
+            graph_cache: Cache::new(),
+        }
+    }
+    pub fn points(
+        &self,
+        range: f32,
+        mid: Vector,
+        resolution: u32,
+        vars: &mut HashMap<SubscriptSymbol, f32>,
+    ) -> Result<Vec<Option<Vector>>, String> {
         let mut points = Vec::new();
 
-        let min = self.mid.x - range / 2.0;
-        let dx = range / self.resolution as f32;
+        let min = mid.x - range / 2.0;
+        let dx = range / resolution as f32;
+        vars.insert(SubscriptSymbol::from('x'), 0.0);
 
-        for i in 0..self.resolution {
+        for i in 0..resolution {
             let x = i as f32 * dx + min;
-            let point = Vector { x, y: (self.f)(x) };
+            vars.get_mut(&SubscriptSymbol::from('x')).map(|v| *v = x);
+            let point = match self.eq.evaluate(vars) {
+                Ok(y) => Some(Vector { x, y }),
+                Err(EvalError::DivideByZeroError) => None,
+                Err(EvalError::UnknownSymbol(s)) => return Err(format!("unknown symbol: {:?}", s)),
+            };
             points.push(point);
         }
 
-        points
+        Ok(points)
+    }
+
+    pub fn clear_cache(&self) {
+        self.graph_cache.clear()
     }
 }
 
@@ -49,10 +108,7 @@ impl Default for GraphState {
     }
 }
 
-impl<F> Program<Message> for Graph<F>
-where
-    F: Fn(f32) -> f32,
-{
+impl Program<Message> for GraphRenderer {
     type State = GraphState;
     fn draw(
         &self,
@@ -62,28 +118,50 @@ where
         bounds: iced::Rectangle,
         cursor: Cursor,
     ) -> Vec<Geometry> {
-        let mut frame = Frame::new(renderer, bounds.size());
+        let graphs = self.graphs.iter().filter_map(|g| g.as_ref()).map(|graph| {
+            graph.graph_cache.draw(renderer, bounds.size(), |frame| {
+                let mut symbols = self.symbols.clone();
 
-        let points = self.get_points(bounds.width / self.scale);
-        for i in 0..points.len() - 1 {
-            let (point, next_point) = (points[i], points[i + 1]);
+                match graph.points(
+                    bounds.width / self.scale,
+                    self.mid,
+                    self.resolution,
+                    &mut symbols,
+                ) {
+                    Ok(points) => {
+                        for i in 0..points.len() - 1 {
+                            match (points[i], points[i + 1]) {
+                                (Some(point), Some(next_point)) => {
+                                    frame.stroke(
+                                        &Path::line(
+                                            Point {
+                                                x: (point.x - self.mid.x) * self.scale as f32
+                                                    + bounds.width / 2.0,
+                                                y: (point.y - self.mid.y) * self.scale as f32
+                                                    + bounds.height / 2.0,
+                                            },
+                                            Point {
+                                                x: (next_point.x - self.mid.x) * self.scale as f32
+                                                    + bounds.width / 2.0,
+                                                y: (next_point.y - self.mid.y) * self.scale as f32
+                                                    + bounds.height / 2.0,
+                                            },
+                                        ),
+                                        Stroke::default().with_width(4.0),
+                                    );
+                                }
+                                _ => (),
+                            }
+                        }
+                    }
+                    Err(e) => eprintln!("{}", e),
+                }
+            })
+        });
 
-            frame.stroke(
-                &Path::line(
-                    Point {
-                        x: (point.x - self.mid.x) * self.scale as f32 + bounds.width / 2.0,
-                        y: (point.y - self.mid.y) * self.scale as f32 + bounds.height / 2.0,
-                    },
-                    Point {
-                        x: (next_point.x - self.mid.x) * self.scale as f32 + bounds.width / 2.0,
-                        y: (next_point.y - self.mid.y) * self.scale as f32 + bounds.height / 2.0,
-                    },
-                ),
-                Stroke::default().with_width(4.0),
-            );
-        }
+        let graphs = graphs.collect();
 
-        vec![frame.into_geometry()]
+        graphs
     }
 
     fn update(
@@ -122,6 +200,26 @@ where
                         (event::Status::Captured, Some(Message::Moved(diff)))
                     }
                     GraphState::None => (event::Status::Ignored, None),
+                },
+                mouse::Event::WheelScrolled { delta } => match delta {
+                    mouse::ScrollDelta::Lines { y, .. } | mouse::ScrollDelta::Pixels { y, .. } => {
+                        let scaling = self.scale * (1.0 + y / 30.0);
+                        let mid =
+                            if let Some(cursor_to_center) = cursor.position_from(bounds.center()) {
+                                let factor = scaling - self.scale;
+
+                                Some(
+                                    self.mid
+                                        - Vector::new(
+                                            cursor_to_center.x * factor / (self.scale * self.scale),
+                                            cursor_to_center.y * factor / (self.scale * self.scale),
+                                        ),
+                                )
+                            } else {
+                                None
+                            };
+                        (event::Status::Captured, Some(Message::Scaled(scaling, mid)))
+                    }
                 },
                 _ => (event::Status::Ignored, None),
             },
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use graph::Graph;
+use components::equations;
+use graph::GraphRenderer;
 use iced::{
-    subscription,
-    widget::{column, container, text, Canvas},
-    Application, Command, Length, Point, Settings, Vector,
+    widget::{
+        row,
+        text_input::{focus, Id},
+        Canvas,
+    },
+    Application, Command, Length, Settings, Vector,
 };
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsValue;
 
+mod components;
 mod graph;
+mod latex;
+mod parser;
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    Somsed::run(Settings {
+        antialiasing: true,
+        ..Default::default()
+    })
+    .map_err(|e| JsValue::from_str(&e.to_string()))
+}
 
 fn main() {
     Somsed::run(Settings {
@@ -15,17 +34,18 @@ fn main() {
     .unwrap();
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Message {
     Moved(Vector),
-}
-
-fn function(x: f32) -> f32 {
-    return x * x;
+    Scaled(f32, Option<Vector>),
+    EquationChanged(usize, String),
+    EquationAdded(String),
+    EquationMessage(equations::Message),
 }
 
 struct Somsed {
-    graph: Graph<fn(f32) -> f32>,
+    graph_renderer: GraphRenderer,
+    equations: equations::Equations,
 }
 
 impl Application for Somsed {
@@ -34,33 +54,54 @@ impl Application for Somsed {
     type Theme = iced::Theme;
     type Flags = ();
 
-    fn new(_: ()) -> (Self, Command<Message>) {
+    fn new(_: Self::Flags) -> (Self, Command<Message>) {
         (
             Self {
-                graph: Graph {
-                    f: |x| x.sin(),
-                    scale: 100.0,
-                    mid: Vector { x: 0.0, y: 0.0 },
-                    resolution: 1000,
-                },
+                graph_renderer: GraphRenderer::new(100.0, Vector { x: 0.0, y: 0.0 }, 1000),
+                equations: Default::default(),
             },
             Command::none(),
         )
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        column![Canvas::new(&self.graph)
+        let content = Canvas::new(&self.graph_renderer)
             .width(Length::Fill)
-            .height(Length::Fill)]
-        .into()
+            .height(Length::Fill);
+
+        row![self.equations.view(), content]
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
     }
 
     fn update(&mut self, message: Self::Message) -> iced::Command<Message> {
         match message {
             Message::Moved(p) => {
-                self.graph.mid = self.graph.mid + p;
+                self.graph_renderer.mid = self.graph_renderer.mid + p;
+                self.graph_renderer.clear_caches();
                 iced::Command::none()
             }
+            Message::EquationChanged(i, s) => {
+                let err = self.graph_renderer.set_equation(i, &s).err();
+                self.equations.set_equation(i, s, err);
+
+                iced::Command::none()
+            }
+            Message::EquationAdded(s) => {
+                let err = self.graph_renderer.add_equation(&s).err();
+                self.equations.add_equation(s, err);
+                iced::Command::none()
+            }
+            Message::Scaled(scale, mid) => {
+                self.graph_renderer.scale = scale;
+                if let Some(mid) = mid {
+                    self.graph_renderer.mid = mid;
+                }
+                self.graph_renderer.clear_caches();
+                iced::Command::none()
+            }
+            Message::EquationMessage(m) => self.equations.update(m),
         }
     }
 
